7cb026329b8e1f68fc266a9133e254f245335577
birdhui <tpgml6817@naver.com>
2023-06-03 14:25:54 +0900

    Chapter06_page23_Review

---
+++ ch6_p21.cpp
@@ -0,0 +1,31 @@
+// 객체 포인터 선언 및 활용
+#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle() { radius = 1; }
+	Circle(int r) { radius = r; }
+	double getArea();
+};
+
+double Circle::getArea() {
+	return 3.14 * radius * radius;
+}
+
+int main() {
+	Circle donut;
+	Circle pizza(30);
+
+	cout << donut.getArea() << endl;
+
+	Circle * p;
+	p = &donut;
+	cout << p->getArea() << endl;		// 포인터로 멤버함수에 접근할 때 -> 화살표로 접근
+	cout << (*p).getArea() << endl;
+
+	p = &pizza;
+	cout << p->getArea() << endl;
+	cout << (*p).getArea() << endl;
+}
